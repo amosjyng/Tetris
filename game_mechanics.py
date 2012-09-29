3d56598b70f193e6d2c75628b8c5b63abd125992
Amos Ng <amosjyng@gmail.com>
2012-09-29 15:46:14 -0700

    Refactored many of the classes into different files.

---
+++ game_mechanics.py
@@ -0,0 +1,23 @@
+SCALE = 20
+OFFSET = 3
+MAXX = 10
+MAXY = 22
+
+NO_OF_LEVELS = 10
+
+LEFT = "left"
+RIGHT = "right"
+DOWN = "down"
+
+direction_d = { "left": (-1, 0), "right": (1, 0), "down": (0, 1) }
+
+def level_thresholds( first_level, no_of_levels ):
+    """
+    Calculates the score at which the level will change, for n levels.
+    """
+    thresholds =[]
+    for x in xrange( no_of_levels ):
+        multiplier = 2**x
+        thresholds.append( first_level * multiplier )
+
+    return thresholds
