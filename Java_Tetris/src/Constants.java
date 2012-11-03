fec44712c8eec48b7c08980ceef68e00c12898cf
Amos Ng <amosjyng@gmail.com>
2012-11-03 15:17:49 -0700

    Made it ready for packaging into a jar.

--- Java_Tetris/src/Constants.java
+++ Java_Tetris/src/Constants.java
@@ -2,7 +2,7 @@ public class Constants
 {
     public static final int MAXX = 10;
     public static final int MAXY = 22;
-    public static final int SHAPES_QUEUE_SIZE = 1;
+    public static final int SHAPES_QUEUE_SIZE = 2;
     public static final int MAX_LEVELS = 10;
     public static final boolean LINEAR_SCORE_FUNCTION = true;
     public static final int SCORE_MULTIPLIER = 1; // make people happier, I guess
@@ -12,7 +12,7 @@ public class Constants
     public static final String REMOVED_SQUARE_COLOR = "gray";
 
     // AI related stuff
-    public static final int AI_TRIALS = 4;
+    public static final int AI_TRIALS = 20;
     public static final int AI_PAUSE_INTERVAL = 0;
     public static final double CURRENT_MOVE_MULTIPLIER = 1.01; // prioritize earlier gains
     public static final double GAME_SCORE_WEIGHT = 10;
